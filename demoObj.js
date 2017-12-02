608b9999f021ac1c4ea15eb4352c72a7e472545b
Philip N <phi.nguyen@laverne.edu>
2017-12-01 21:00:06 -0800

    Add ex of author not in db in demoObj.js

--- demoObj.js
+++ demoObj.js
@@ -1,14 +1,25 @@
 {
-  "_id": ObjectId("5a1f4bf9fb061a1c943fca2b"),
-  "name": "Building",
+  "_id": ObjectId("5a1f4b7817b403971c0639c0"),
+  "name": "architecture",
   "image": "https://placeimg.com/640/480/arch",
-  "description": "sadf",
-  "comments": [ObjectId("5a1f4e91fb061a1c943fca2c")],
+  "description": "desc",
+  "comments": [],
   "author": {
-    "id": ObjectId("5a1f4be8fb061a1c943fca2a"),
-    "username": "Mike"
+    "id": ObjectId("5a1f486117b403971c0639bc"),
+    "username": "Phil"
   },
-  "__v": 1
+  "__v": 0
+}
+
+
+
+// does not show author object in db:
+{
+  "_id": ObjectId("5a223175a1985d5dc8140a99"),
+  "text": "from mike",
+  "submittedDate": "Fri Dec 01 2017",
+  "submittedTime": "8:52:05 PM",
+  "__v": 0
 }
 
 
@@ -20,19 +31,39 @@
 }
 
 
-[ { _id: 5a1f4e91fb061a1c943fca2c,
-   text: 'hey eja;lkdsfj',
-   __v: 0,
-   author: { id: 5a1f4be8fb061a1c943fca2a, username: 'Mike' } } ]
-allComments [ { _id: 5a1f4e91fb061a1c943fca2c,
-   text: 'hey eja;lkdsfj',
-   __v: 0,
-   author: { id: 5a1f4be8fb061a1c943fca2a, username: 'Mike' } } ]
-allComments [ { _id: 5a1f4e91fb061a1c943fca2c,
-   text: 'hey eja;lkdsfj',
-   __v: 0,
-   author: { id: 5a1f4be8fb061a1c943fca2a, username: 'Mike' } } ]
-allComments [ { _id: 5a1f4e91fb061a1c943fca2c,
-   text: 'hey eja;lkdsfj',
-   __v: 0,
-   author: { id: 5a1f4be8fb061a1c943fca2a, username: 'Mike' } } ]
+[{
+  _id: 5 a1f4e91fb061a1c943fca2c,
+  text: 'hey eja;lkdsfj',
+  __v: 0,
+  author: {
+    id: 5 a1f4be8fb061a1c943fca2a,
+    username: 'Mike'
+  }
+}]
+allComments[{
+  _id: 5 a1f4e91fb061a1c943fca2c,
+  text: 'hey eja;lkdsfj',
+  __v: 0,
+  author: {
+    id: 5 a1f4be8fb061a1c943fca2a,
+    username: 'Mike'
+  }
+}]
+allComments[{
+  _id: 5 a1f4e91fb061a1c943fca2c,
+  text: 'hey eja;lkdsfj',
+  __v: 0,
+  author: {
+    id: 5 a1f4be8fb061a1c943fca2a,
+    username: 'Mike'
+  }
+}]
+allComments[{
+  _id: 5 a1f4e91fb061a1c943fca2c,
+  text: 'hey eja;lkdsfj',
+  __v: 0,
+  author: {
+    id: 5 a1f4be8fb061a1c943fca2a,
+    username: 'Mike'
+  }
+}]
