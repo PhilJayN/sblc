d1f195517eb1d1388e88ec94a4e4b67b18ab90f8
Philip N <phi.nguyen@laverne.edu>
2017-12-03 23:30:42 -0800

    Add comment id route skeleton

--- demoObj.js
+++ demoObj.js
@@ -1,33 +1,53 @@
 {
-  "_id": ObjectId("5a1f4b7817b403971c0639c0"),
-  "name": "architecture",
-  "image": "https://placeimg.com/640/480/arch",
-  "description": "desc",
-  "comments": [],
+  "_id": ObjectId("5a22360f457b205e7c16ee16"),
+  "text": "heeeyjalkdfj there",
+  "submittedDate": "Fri Dec 01 2017",
+  "submittedTime": "9:11:43 PM",
   "author": {
-    "id": ObjectId("5a1f486117b403971c0639bc"),
+    "id": ObjectId("5a1f4be8fb061a1c943fca2a"),
+    "username": "Mike"
+  },
+  "__v": 0
+} {
+  "_id": ObjectId("5a223652457b205e7c16ee17"),
+  "text": "this is cool",
+  "submittedDate": "Fri Dec 01 2017",
+  "submittedTime": "9:12:50 PM",
+  "author": {
+    "id": ObjectId("5a1f4be8fb061a1c943fca2a"),
+    "username": "Mike"
+  },
+  "__v": 0
+} {
+  "_id": ObjectId("5a22369e457b205e7c16ee19"),
+  "text": "hellooo",
+  "submittedDate": "Fri Dec 01 2017",
+  "submittedTime": "9:14:06 PM",
+  "author": {
+    "id": ObjectId("5a223690457b205e7c16ee18"),
     "username": "Phil"
   },
   "__v": 0
-}
-
-
-
-// does not show author object in db:
-{
-  "_id": ObjectId("5a223175a1985d5dc8140a99"),
-  "text": "from mike",
-  "submittedDate": "Fri Dec 01 2017",
-  "submittedTime": "8:52:05 PM",
+} {
+  "_id": ObjectId("5a231ce17e399326e8474cd3"),
+  "text": "this if fudge!!",
+  "submittedDate": "Sat Dec 02 2017",
+  "submittedTime": "1:36:33 PM",
+  "author": {
+    "id": ObjectId("5a223690457b205e7c16ee18"),
+    "username": "Phil"
+  },
   "__v": 0
-}
-
-
-{
-  "name": "Building",
-  "image": "https://placeimg.com/640/480/arch",
-  "description": "sadf",
-  "__v": 1
+} {
+  "_id": ObjectId("5a23420e5490024ea845890c"),
+  "text": "Hi!!",
+  "submittedDate": "Sat Dec 02 2017",
+  "submittedTime": "4:15:10 PM",
+  "author": {
+    "id": ObjectId("5a223690457b205e7c16ee18"),
+    "username": "Phil"
+  },
+  "__v": 0
 }
 
 
