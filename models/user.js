73042af944c40ca11648648adb4cb37b66e7f8fc
Philip N <phi.nguyen@laverne.edu>
2017-12-05 16:11:32 -0800

    Update user model

--- models/user.js
+++ models/user.js
@@ -3,7 +3,13 @@ var passportLocalMongoose = require("passport-local-mongoose");
 
 var UserSchema = new mongoose.Schema({
   username: String,
-  password: String //fix typo on left!!
+  password: String, //fix typo on left!!
+  firstName: {type: String, default: "First Name"},
+  lastName: {type: String, default: "Last Name"},
+  email: String,
+  avatar: {type: String, default: "https://upload.wikimedia.org/wikipedia/en/b/b1/Portrait_placeholder.png"},
+  createdAt: {type: String, default: Date.now},
+  isAdmin: {type: Boolean, default: false}
 });
 
 UserSchema.plugin(passportLocalMongoose);
