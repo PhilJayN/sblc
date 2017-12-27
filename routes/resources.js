f471c82a4b5f91d51daa3a9a9c82fd56f1f24f98
Philip N <phi.nguyen@laverne.edu>
2017-12-26 21:52:43 -0800

    Edit route for resources pg

--- routes/resources.js
+++ routes/resources.js
@@ -1,13 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/cat', function(req, res) {
-    res.send('resources pg newsadfasdf');
+router.get('/resources', function(req, res) {
+    res.render('resources/index.ejs');
 });
 
-
-// router.get('/cat', function(req, res) {
-//     res.send('resources pg new');
-// });
 
 module.exports = router;
