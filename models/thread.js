b7e6bac346e8a0a446742d1b37e781a320918546
Philip N <phi.nguyen@laverne.edu>
2017-12-29 21:56:47 -0800

    Add basic skeleton for threads

---
+++ models/thread.js
@@ -0,0 +1,19 @@
+var mongoose = require("mongoose");
+
+var threadSchema = mongoose.Schema({
+  subject: String,
+  text: String,
+  isCollapsed: {type: Boolean, default: false},
+  author:
+  {
+      id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+      },
+      username: String
+  },
+  submittedDate: String,
+  submittedTime: String
+});
+
+module.exports = mongoose.model("Thread", threadSchema);
