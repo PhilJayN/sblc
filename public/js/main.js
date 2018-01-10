7c232415cec80a8a14e0303ae3de068e5da9fa4d
Philip N <phi.nguyen@laverne.edu>
2018-01-09 20:55:10 -0800

    Add basic skeleton for main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,30 @@
-console.log('hi there!');
+console.log('hi this is the SBLC app!');
+
+var listController = (function() {
+  //calculation and data structure goes here.
+})();
+
+// code for displaying or updating UI here:
+var UIController = (function() {
+
+  var DOMstrings = {
+    tempText: '.tempText'
+  };
+
+})();
+
+// GLOBAL APP CONTROLLER: main job is to call other methods
+var controller = (function(listCtrl, UICtrl) {
+
+  var setupEventListeners = function() {
+    var DOM = UICtrl.getDOMstrings();
+
+  };
+
+  var ctrlAddItem = function(event) {
+    console.log ('a click event caused ctrlAddItem to be called!');
+  }
+
+})(listController, UIController);
+
+controller.init();
