e859c983e3fc9728e2b9b0ae4ec53952ae8965c9
Philip N <phi.nguyen@laverne.edu>
2018-01-10 21:27:49 -0800

    Remove extra classes from threads section

--- public/js/main.js
+++ public/js/main.js
@@ -1,36 +1,60 @@
-console.log('hi this is the SBLC app!');
+// console.log('hi this is the SBLC app!');
+//
+// var listController = (function() {
+//   //calculation and data structure goes here.
+// })();
+//
+// // code for displaying or updating UI here:
+// var UIController = (function() {
+//
+//   var DOMstrings = {
+//     toggleBtn: '.toggle-btn'
+//   };
+//
+//   return {
+//     getDOMstrings: function() {
+//       return DOMstrings;
+//     }
+//   }
+//
+// })();
+//
+// // GLOBAL APP CONTROLLER: main job is to call other methods
+// var controller = (function(listCtrl, UICtrl) {
+//
+//   var setupEventListeners = function() {
+//     var DOM = UICtrl.getDOMstrings();
+//     // document.querySelector(DOM.toggleBtn).addEventListener('click', )
+//   };
+//
+//   var ctrlAddItem = function(event) {
+//     console.log ('a click event caused ctrlAddItem to be called!');
+//   }
+//
+// })(listController, UIController);
+//
+// controller.init();
+//
 
-var listController = (function() {
-  //calculation and data structure goes here.
-})();
 
-// code for displaying or updating UI here:
-var UIController = (function() {
+$(document).ready(function(){
 
-  var DOMstrings = {
-    toggleBtn: '.toggle-btn'
-  };
+  //  $('.collapseAll').click(function(){
+  //      $('.panel-collapse.in')
+	// 	.collapse('hide');
+  //   });
+  //   $('.expandAll').click(function(){
+	// $('.panel-collapse:not(".in")')
+	// 	.collapse('show');
+  //   });
 
-  return {
-    getDOMstrings: function() {
-      return DOMstrings;
-    }
-  }
+console.log ('hi!!');
 
-})();
+    $('.btn-main-controller').click(function() {
+      console.log (' you clicked!!');
+      $('.multi-collapse').collapse('hide');
 
-// GLOBAL APP CONTROLLER: main job is to call other methods
-var controller = (function(listCtrl, UICtrl) {
 
-  var setupEventListeners = function() {
-    var DOM = UICtrl.getDOMstrings();
-    // document.querySelector(DOM.toggleBtn).addEventListener('click', )
-  };
+    });
 
-  var ctrlAddItem = function(event) {
-    console.log ('a click event caused ctrlAddItem to be called!');
-  }
-
-})(listController, UIController);
-
-controller.init();
+});
