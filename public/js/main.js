b0c63f86e4b17b28cf84a89f631209160884445b
Philip N <phi.nguyen@laverne.edu>
2018-01-23 23:11:11 -0800

    Clean up main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,40 +1,39 @@
-// console.log('hi this is the SBLC app!');
-//
-// var listController = (function() {
-//   //calculation and data structure goes here.
-// })();
-//
-// // code for displaying or updating UI here:
-// var UIController = (function() {
-//
-//   var DOMstrings = {
-//     toggleBtn: '.toggle-btn'
-//   };
-//
-//   return {
-//     getDOMstrings: function() {
-//       return DOMstrings;
-//     }
-//   }
-//
-// })();
-//
-// // GLOBAL APP CONTROLLER: main job is to call other methods
-// var controller = (function(listCtrl, UICtrl) {
-//
-//   var setupEventListeners = function() {
-//     var DOM = UICtrl.getDOMstrings();
-//     // document.querySelector(DOM.toggleBtn).addEventListener('click', )
-//   };
-//
-//   var ctrlAddItem = function(event) {
-//     console.log ('a click event caused ctrlAddItem to be called!');
-//   }
-//
-// })(listController, UIController);
-//
-// controller.init();
-//
+console.log('hi this is the SBLC app!');
+
+var listController = (function() {
+  //calculation and data structure goes here.
+})();
+
+// code for displaying or updating UI here:
+var UIController = (function() {
+
+  var DOMstrings = {
+    toggleBtn: '.toggle-btn'
+  };
+
+  return {
+    getDOMstrings: function() {
+      return DOMstrings;
+    }
+  }
+
+})();
+
+// GLOBAL APP CONTROLLER: main job is to call other methods
+var controller = (function(listCtrl, UICtrl) {
+
+  var setupEventListeners = function() {
+    var DOM = UICtrl.getDOMstrings();
+    // document.querySelector(DOM.toggleBtn).addEventListener('click', )
+  };
+
+  var ctrlAddItem = function(event) {
+    console.log ('a click event caused ctrlAddItem to be called!');
+  }
+
+})(listController, UIController);
+
+controller.init();
 
 
 $(document).ready(function(){
@@ -48,23 +47,10 @@ $(document).ready(function(){
 	// 	.collapse('show');
   //   });
 
-console.log ('hi!sdafasdf!');
-
     $('.btn-main-controller').click(function() {
       console.log (' you clicked!!');
       $('.multi-collapse').collapse('hide');
     });
-
-
-//     var email = document.getElementById("mail");
-//     email.addEventListener("input", function (event) {
-//   if (email.validity.typeMismatch) {
-//     email.setCustomValidity("I expect an e-mail, darling!");
-//   } else {
-//     email.setCustomValidity("");
-//   }
-// });
-
 
   var subject = document.getElementById('subject');
   var submitThreadBtn = document.getElementById('submit-thread');
@@ -72,7 +58,6 @@ console.log ('hi!sdafasdf!');
   document.querySelector('.submit-btn').addEventListener('click', validateInput);
 
   // document.querySelector(DOM.assortedBtn).addEventListener('click', ctrlAddItem);
-
 
   submitThreadBtn.addEventListener('click', function(event) {
     if (subject.validity.valueMissing) {
@@ -82,13 +67,6 @@ console.log ('hi!sdafasdf!');
 
   function validateInput() {
     console.log ('you clicked!!');
-    
   }
-
-  // subject.addEventListener('input', function (event) {
-  //   if (subject.validity.valueMissing) {
-      // subject.setCustomValidity('Please write a subject.');
-  //   }
-  // });
 
 });
