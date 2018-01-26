fc2cbaf2e79f6f1f0840366b779e59886f71c5e9
Philip N <phi.nguyen@laverne.edu>
2018-01-25 22:47:16 -0800

    Move orders of bootstrap popper js files

--- public/js/demo.js
+++ public/js/demo.js
@@ -1,5 +1,7 @@
 console.log('demo JavaScript file');
 
+var elDemo = document.querySelector('.demo-class');
+console.log('elDemo', elDemo);
 
 document.querySelector('.demo-class').addEventListener('click', function(event) {
 console.log ('you clicked demo-class el');
