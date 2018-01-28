8fb1b081ec8347029d01ec017ec2f3b321cc162e
Philip N <phi.nguyen@laverne.edu>
2018-01-27 21:49:45 -0800

    Update comment delete confirm

--- public/js/main.js
+++ public/js/main.js
@@ -91,9 +91,12 @@ var controller = (function(listCtrl, UICtrl) {
   var ctrlDelItem = function(event) {
     console.log ('DELETE!');
     //1. asks user to Confirm
-
-
     //2. use confirmation str, then delete.
+    var userAns = prompt('Please type in yes, then press DELETE.');
+    if (userAns.toLowerCase() === 'yes') {
+      console.log ('asdfjkl;');
+    }
+
 
 
   };
