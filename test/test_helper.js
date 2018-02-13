e34e680202cff0ded182371ace64fb126474c8d3
Philip N <phi.nguyen@laverne.edu>
2018-02-12 16:56:25 -0800

    Add beforeEach hook to test_helper

--- test/test_helper.js
+++ test/test_helper.js
@@ -6,3 +6,12 @@ mongoose.connection
     .on('error', (error) => {
       console.warn('Warning', error);
     });
+
+
+//hook (ex: beforeEach): a function that will be exec before any tests get executed inside our test suite
+beforeEach((done) => { //"done" is provided by mocha
+  mongoose.connection.collections.users.drop(() => {
+    //ready to run next test, right after db collection has been dropped
+    done();
+  });
+});
