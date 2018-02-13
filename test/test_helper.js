db4c4dc73e86abddaa5aec07da9c63b31cbf9512
Philip N <phi.nguyen@laverne.edu>
2018-02-12 16:35:24 -0800

    Edit test_helper syntax

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,12 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.Promise = global.Promise;
-
-before((done) => {
-  mongoose.connect('mongodb://localhost/users_test');
-  mongoose.connection
-    .once('open', () => { done(); })
+mongoose.connect('mongodb://localhost/users_test');
+mongoose.connection
+    .once('open', () => console.log('Good to go!'))
     .on('error', (error) => {
       console.warn('Warning', error);
     });
-});
