52737dacd944edcc5713a23238115f2091abb23a
Philip N <phi.nguyen@laverne.edu>
2018-02-12 16:25:26 -0800

    Add example ES6 example of test_helper

--- test/create_test.js
+++ test/create_test.js
@@ -1,7 +1,34 @@
+// var assert = require('assert');
+//
+// describe('Creating records', function() {
+//   it('saves a user', function() {
+//       assert(1+1 === 2);
+//   });
+// });
+
 var assert = require('assert');
+var User = require('../models/user.js');
+var Reply = require('../models/reply.js');
+
 
 describe('Creating records', function() {
   it('saves a user', function() {
-      assert(1+1 === 2);
+    var joe = new User({username: 'JoeBlow'}); //joe is NOT yet saved to db.
+    //joe is an object, which has many methods, one of which is .save()
+    //now to save to db:
+    joe.save();
   });
 });
+
+
+describe('Creating records', function() {
+  it('saves a reply', function() {
+    var dinner = new Reply({text: 'Food!'});
+    console.log(dinner, 'type:', typeof dinner);
+    dinner.save();
+  });
+});
+
+
+// console.log('joe type', typeof joe);
+// assert(joe === 'JoeBlow' );
