03dd0c296f7264bbde53c72607adac1aeb4bf59f
Philip N <phi.nguyen@laverne.edu>
2018-02-12 19:04:57 -0800

    Add assert with isNew

--- test/create_test.js
+++ test/create_test.js
@@ -1,33 +1,32 @@
-// var assert = require('assert');
-//
+const assert = require('assert');
+const User = require('../models/user.js');
+const Reply = require('../models/reply.js');
+
+describe('Creating records', function() {
+  it('saves a user', function(done) {
+    const joe = new User({username: 'JoeBlow'}); //joe is NOT yet saved to db.
+    //joe is an object, which has many methods, one of which is .save()
+    //now to save to db: (remember .save() also takes a certain amt. of time to run)
+    console.log('isNew b4 save', joe.isNew); //expect true
+
+    joe.save()
+      //has joe been saved Successfully?
+      .then(() =>{
+        assert(!joe.isNew);
+        done();
+        // console.log('isNew after save', joe.isNew); //expect false
+      });
+  });
+});
+
+
 // describe('Creating records', function() {
-//   it('saves a user', function() {
-//       assert(1+1 === 2);
+//   it('saves a reply', function() {
+//     var dinner = new Reply({text: 'Food!'});
+//     console.log(dinner, 'type:', typeof dinner);
+//     dinner.save();
 //   });
 // });
-
-var assert = require('assert');
-var User = require('../models/user.js');
-var Reply = require('../models/reply.js');
-
-
-describe('Creating records', function() {
-  it('saves a user', function() {
-    var joe = new User({username: 'JoeBlow'}); //joe is NOT yet saved to db.
-    //joe is an object, which has many methods, one of which is .save()
-    //now to save to db:
-    joe.save();
-  });
-});
-
-
-describe('Creating records', function() {
-  it('saves a reply', function() {
-    var dinner = new Reply({text: 'Food!'});
-    console.log(dinner, 'type:', typeof dinner);
-    dinner.save();
-  });
-});
 
 
 // console.log('joe type', typeof joe);
