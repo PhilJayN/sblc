ece9d2cc82a17cec6be13c151ac8cd0481351739
Philip N <phi.nguyen@laverne.edu>
2018-05-17 15:39:47 -0700

    Add tests for modifying reply using id method

--- test/nesting_test.js
+++ test/nesting_test.js
@@ -34,25 +34,109 @@ describe('Nesting records w/ sub docs', function() {
   it('Adds replies to a thread', function(done) {
 
     var puppies = new Thread({
-      subject: 'bears are cute',
+      subject: 'monkeys are cute',
       text: 'yes!',
       replies: [{text: 'new reply'}]
     });
 
     puppies.save().then(function() {
-      Thread.findOne({subject: 'bears are cute'}).then(function(record) {
+      Thread.findOne({subject: 'monkeys are cute'}).then(function(record) {
         //now add to the replies array
         record.replies.push({text: 'another reply'});
         record.save().then(function() {
-          Thread.findOne({subject: 'bears are cute'}).then(function(result) {
-            assert(result.replies.length === 2);
+          var id = record._id;
+          Thread.findById(id).then(function(result) {
+            var replyId = result.replies[0]._id;
+            console.log (replyId);
+            result.replies.id(replyId);
+            var modReply = result.replies.id(replyId);
+            console.log ('found reply!!', modReply);
+            modReply.text = '[deleted!!]';
+            result.save();
             done();
           });
-        })
+
+        });
       });
     });
 
   });
 
+  // it('find a thread', function (done) {
+  //
+  //   Thread.findOneAndUpdate({subject: 'parrots are cute'}).then(function(record) {
+  //     console.log('record', record);
+  //     done();
+  //
+  //
+  //   });
+  //
+  // });
 
 });
+
+
+
+
+// WORKS to edit a main reply 
+// it('Adds replies to a thread', function(done) {
+//
+//   var puppies = new Thread({
+//     subject: 'monkeys are cute',
+//     text: 'yes!',
+//     replies: [{text: 'new reply'}]
+//   });
+//
+//   puppies.save().then(function() {
+//     Thread.findOne({subject: 'monkeys are cute'}).then(function(record) {
+//       //now add to the replies array
+//       record.replies.push({text: 'another reply'});
+//       record.save().then(function() {
+//         var id = record._id;
+//         Thread.findById(id).then(function(result) {
+//           var replyId = result.replies[0]._id;
+//           console.log (replyId);
+//           result.replies.id(replyId);
+//           var modReply = result.replies.id(replyId);
+//           console.log ('found reply!!', modReply);
+//           modReply.text = '[deleted!!]';
+//           result.save();
+//           done();
+//         });
+//
+//       });
+//     });
+//   });
+//
+// });
+
+
+
+
+
+
+// working to save to db:
+// it('Adds replies to a thread', function(done) {
+//
+//   var puppies = new Thread({
+//     subject: 'monkeys are cute',
+//     text: 'yes!',
+//     replies: [{text: 'new reply'}]
+//   });
+//
+//   puppies.save().then(function() {
+//     Thread.findOne({subject: 'monkeys are cute'}).then(function(record) {
+//       //now add to the replies array
+//       record.replies.push({text: 'another reply'});
+//       record.save().then(function() {
+//         Thread.findOne({subject: 'monkeys are cute'}).then(function(result) {
+//           assert(result.replies.length === 2);
+//           // console.log ('result: ', result.replies);
+//           console.log ('access',            result.replies[0].text);
+//           done();
+//         });
+//       })
+//     });
+//   });
+//
+// });
